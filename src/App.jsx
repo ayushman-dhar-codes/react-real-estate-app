c8f0a65da2a7a4a7b9d2d752cfe9254521ada57f
Ayushman Dhar <ayushmandhar195@gmail.com>
2025-03-08 13:25:07 +0530

    Updating the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/hero";
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
+import Contact from "./components/Contact/Contact";
 import './App.css';
+import Value from "./components/Value/Value";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -12,6 +16,10 @@ function App() {
         <Hero />
         <Companies />
         <Residencies />
+        <Value />
+        <Contact/>
+        <GetStarted/>
+        <Footer/>
       </div>
     </div>
   );
